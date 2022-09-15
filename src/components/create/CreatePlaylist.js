efdcd10e8529050e50d2197f7f625a770b3fd926
Emma Cohanim <emmacohanim@gmail.com>
2022-09-14 18:41:00 -0700

    forms and such

--- src/components/create/CreatePlaylist.js
+++ src/components/create/CreatePlaylist.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react"
 import Genres from "./Genres"
 import CreatedPlaylist from "./CreatedPlaylist"
+import {Form, Button} from "semantic-ui-react"
 
-function CreatePlaylist( {token}) {
+
+function CreatePlaylist( {token} ) {
 
     const [genres, setGenres] = useState([])
     const [selectedGenres, setSelectedGenres] = useState([]);
+    const [username, setUsername] = useState("")
+    const [playlist, setPlaylist] = useState("")
+    const [songs, setSongs] = useState([])
 
     const handleClick = (genre) => {
         if (selectedGenres.includes(genre)) {
@@ -25,13 +30,49 @@ function CreatePlaylist( {token}) {
         })
         .then(r => r.json())
         .then(genres => setGenres(genres.genres))
-      },[])
+      },[token])
+
+      function handleUsernameChange(e) {
+        e.preventDefault();
+        setUsername(e.target.value)
+      }
+      function handlePlaylistChange(e) {
+        e.preventDefault();
+        setPlaylist(e.target.value)
+      }
+      function handleSubmit(e) {
+        e.preventDefault();
+        let newPlaylist = {Username: username, Playlist: playlist, Songs: songs}
+        console.log(newPlaylist)
+      }
 
     return (
-        <div id="created-playlist">
-            <CreatedPlaylist selectedGenres={selectedGenres} token={token}/>
+        <div id="create-playlist">
             <div id="genre-buttons">
                 <Genres genres={genres} handleClick={handleClick} selectedGenres={selectedGenres}/>
+            </div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group id="name-title-form">
+                    <Form.Input
+                        onChange={handleUsernameChange}
+                        value={username}
+                        type="text"
+                        name="username"
+                        placeholder="Your name..."
+                    /> 
+                    <Form.Input
+                        onChange={handlePlaylistChange}
+                        value={playlist}
+                        type="text"
+                        name="playlist"
+                        placeholder="Playlist name..."
+                    /> 
+                </Form.Group>
+                <Button type='submit'>Submit</Button>
+            </Form>
+            
+            <div id='created-playlist'>
+                <CreatedPlaylist selectedGenres={selectedGenres} token={token} songs={songs} setSongs={setSongs}/>
             </div>
         </div>
         
--- src/components/create/CreatedPlaylist.js
+++ src/components/create/CreatedPlaylist.js
@@ -1,16 +1,9 @@
-import React, {useEffect, useState} from 'react';
-function CreatedPlaylist ({selectedGenres, token}) {
-    // why can't we get tracks?
-    const [songs, setSongs] = useState([])
-
-    function displaySongs() {
-        songs.map((s) => {
-            return <p>{s["name"]}</p>
-        })
-      }
+import React, {useEffect } from 'react';
+function CreatedPlaylist ({selectedGenres, token, songs, setSongs}) {
 
     useEffect(() => {
-        fetch(`https://api.spotify.com/v1/recommendations?limit=5&seed_genres=${selectedGenres.join("%2C")}`, {
+       if (selectedGenres.length>0) {
+         fetch(`https://api.spotify.com/v1/recommendations?limit=20&seed_genres=${selectedGenres.join("%2C")}`, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
@@ -19,12 +12,22 @@ function CreatedPlaylist ({selectedGenres, token}) {
 
         })
         .then(r=>r.json())
-        .then(songs => setSongs(songs.tracks))
-      },[selectedGenres, token])
+        .then(songs => {
+            setSongs(songs.tracks)
+            })
+      }}
+      ,[selectedGenres, token]) 
 
-      
-
-      
+      const displaySongs = songs.map((s) => {
+        console.log(s["artists"][0]["name"])
+        return  (
+            <div>
+                <p>Title: {s["name"]} Artist:{s["artists"][0]["name"]}</p>
+                
+            </div>
+       
+        )
+    })
 
 return (
     <div id="songs-in-playlist">
